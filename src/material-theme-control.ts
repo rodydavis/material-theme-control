813fa84d47df5cf4ad92f1eca548a18f4b530f60
Rody Davis <rody.davis.jr@gmail.com>
2022-08-03 16:03:05 -0700

    adding styles in header and all palettes

--- src/material-theme-control.ts
+++ src/material-theme-control.ts
@@ -6,6 +6,9 @@ import {
   argbFromHex,
   themeFromSourceColor,
   applyTheme,
+  Scheme,
+  hexFromArgb,
+  TonalPalette,
 } from "@material/material-color-utilities";
 
 export const tagName = "material-theme-control";
@@ -95,12 +98,11 @@ export class MaterialThemeControl extends LitElement {
   `;
 
   @state() showOptions = false;
-  @property({ type: Boolean }) dark =
-    localStorage.getItem("theme-dark") === "true";
   @property() color = localStorage.getItem("theme-color") || "#6750A4";
   @query("#theme-options") options!: HTMLDivElement;
 
   render() {
+    const dark = document.body.classList.contains("dark-theme");
     return html` <mwc-icon-button @click=${this.toggleOptions}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -145,8 +147,8 @@ export class MaterialThemeControl extends LitElement {
           <div class="row option">
             <label for="brightness">Brightness</label>
             <mwc-icon-button id="brightness" @click=${this.toggle}>
-              ${!this.dark
-                ? html`<svg
+              ${!dark
+        ? html`<svg
                     class="icon"
                     viewBox="0 0 48 48"
                     xmlns="http://www.w3.org/2000/svg"
@@ -155,7 +157,7 @@ export class MaterialThemeControl extends LitElement {
                       d="M24 42Q16.5 42 11.25 36.75Q6 31.5 6 24Q6 16.5 11.25 11.25Q16.5 6 24 6Q24.4 6 24.85 6.025Q25.3 6.05 26 6.1Q24.2 7.7 23.2 10.05Q22.2 12.4 22.2 15Q22.2 19.5 25.35 22.65Q28.5 25.8 33 25.8Q35.6 25.8 37.95 24.875Q40.3 23.95 41.9 22.3Q41.95 22.9 41.975 23.275Q42 23.65 42 24Q42 31.5 36.75 36.75Q31.5 42 24 42ZM24 39Q29.45 39 33.5 35.625Q37.55 32.25 38.55 27.7Q37.3 28.25 35.875 28.525Q34.45 28.8 33 28.8Q27.25 28.8 23.225 24.775Q19.2 20.75 19.2 15Q19.2 13.8 19.45 12.425Q19.7 11.05 20.35 9.3Q15.45 10.65 12.225 14.775Q9 18.9 9 24Q9 30.25 13.375 34.625Q17.75 39 24 39ZM23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Q23.8 24.15 23.8 24.15Z"
                     />
                   </svg>`
-                : html`<svg
+        : html`<svg
                     class="icon"
                     viewBox="0 0 48 48"
                     xmlns="http://www.w3.org/2000/svg"
@@ -183,9 +185,13 @@ export class MaterialThemeControl extends LitElement {
   }
 
   private toggle() {
-    this.dark = !this.dark;
-    localStorage.setItem("theme-dark", this.dark.toString());
-    this.updateTheme();
+    const dark = document.body.classList.contains("dark-theme");
+    if (!dark) {
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+    }
+    this.requestUpdate();
   }
 
   private setColor(val: string) {
@@ -217,35 +223,125 @@ export class MaterialThemeControl extends LitElement {
 
   private updateTheme() {
     const source = this.color;
-    const dark = this.dark;
-    if (this.dark) {
+    const target = this.shadowRoot!.querySelector("main") as HTMLElement;
+    const theme = themeFromSourceColor(argbFromHex(source));
+    // const tones = [100, 99, 98, 95, 90, 80, 70, 60, 50, 40, 35, 30, 25, 20, 10, 0];
+    const tones = Array.from(Array(101).keys());
+    let sb: string[] = [];
+    sb.push(`:root {`);
+    // sb.push(` color-scheme: light dark;`);
+
+    // Schemes
+    const applyScheme = (scheme: Scheme, options?: {
+      suffix?: string,
+      apply?: boolean,
+    }) => {
+      for (const [key, value] of Object.entries(scheme.toJSON())) {
+        const token = key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+        const color = hexFromArgb(value);
+        const tokenName = `--md-sys-color-${token}${options?.suffix ?? ''}`;
+        if (options?.apply) {
+          target.style.setProperty(tokenName, color);
+        } else {
+          sb.push(`  ${tokenName}: ${color};`);
+        }
+      }
+    };
+
+    // Light theme
+    applyScheme(theme.schemes.light, { suffix: '-light' });
+
+    // Dark theme
+    applyScheme(theme.schemes.dark, { suffix: '-dark' });
+
+    sb.push(`}`);
+
+    // Apply light theme
+    sb.push(`:root, .light-theme {`);
+    sb.push(`  color-scheme: light;`);
+    applyScheme(theme.schemes.light);
+    sb.push(`}`);
+
+    // Set dark theme
+    sb.push(`.dark-theme {`);
+    sb.push(`  color-scheme: dark;`);
+    applyScheme(theme.schemes.dark);
+    sb.push(`}`);
+
+    this.findStyleTag('theme').innerHTML = sb.join("\n");
+
+    // Palettes
+    const applyPalette = (palette: TonalPalette, group: string, tones: number[], options?: {
+      apply?: boolean,
+    }) => {
+      const sb: string[] = [];
+      sb.push(`:root {`);
+      for (const tone of tones) {
+        const raw = palette.tone(tone);
+        const tokenName = `--md-ref-palette-${group}${tone}`;
+        const color = hexFromArgb(raw);
+
+        if (options?.apply) {
+          target.style.setProperty(tokenName, color);
+        } else {
+          sb.push(`  ${tokenName}: ${color};`);
+        }
+      }
+      sb.push(`}`);
+      return sb.join("\n");
+    };
+
+    // Primary
+    const primaryPalette = applyPalette(theme.palettes.primary, "primary", tones);;
+    this.findStyleTag('palette-primary').innerHTML = primaryPalette;
+
+    // Secondary
+    const secondaryPalette = applyPalette(theme.palettes.secondary, "secondary", tones);;
+    this.findStyleTag('palette-secondary').innerHTML = secondaryPalette;
+
+    // Tertiary
+    const tertiaryPalette = applyPalette(theme.palettes.tertiary, "tertiary", tones);
+    this.findStyleTag('palette-tertiary').innerHTML = tertiaryPalette;
+
+    // Neutral
+    const neutralPalette = applyPalette(theme.palettes.neutral, "neutral", tones);
+    this.findStyleTag('palette-neutral').innerHTML = neutralPalette;
+
+    // Neutral Variant
+    const neutralVariantPalette = applyPalette(theme.palettes.neutralVariant, "neutral-variant", tones);
+    this.findStyleTag('palette-neutral-variant').innerHTML = neutralVariantPalette;
+
+  }
+
+  private findStyleTag(id: string) {
+    const styleId = `generated-material-${id}`;
+    let style = document.getElementById(styleId);
+    if (style == null) {
+      style = document.createElement("style");
+      style.id = styleId;
+      document.head.appendChild(style);
+    }
+    return style;
+  }
+
+  private toggleClass(dark: boolean) {
+    if (dark) {
       document.body.classList.add("dark-theme");
     } else {
       document.body.classList.remove("dark-theme");
     }
-    const target = this.shadowRoot!.querySelector("main") as HTMLElement;
-    const theme = themeFromSourceColor(argbFromHex(source));
-    applyTheme(theme, {
-      target,
-      dark,
-      brightnessSuffix: true,
-      paletteTones: [100, 99, 98, 95, 90, 80, 70, 60, 50, 40, 35, 30, 25, 20, 10, 0],
-    });
+    this.requestUpdate();
   }
 
   firstUpdated() {
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
-    const dark =
-      localStorage.getItem("theme-dark") ?? prefersDark.matches.toString();
-    this.dark = dark === "true";
-    if (this.dark) {
-      document.body.classList.add("dark-theme");
-    }
     this.updateTheme();
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    this.toggleClass(prefersDark.matches);
     prefersDark.addEventListener("change", (e) => {
-      this.dark = e.matches;
-      this.updateTheme();
+      const dark = e.matches;
+      this.toggleClass(dark);
     });
+
   }
 }
 
@@ -254,3 +350,4 @@ declare global {
     "material-theme-control": MaterialThemeControl;
   }
 }
+
